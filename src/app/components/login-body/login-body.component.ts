d3025fc7df83b9b11bc65c45bd8ff9bcec3ace32
narekavdalyan <narek.avdalyan.2021@mail.ru>
2025-07-17 15:31:37 +0400

    register & login page some logic and validators

--- src/app/components/login-body/login-body.component.ts
+++ src/app/components/login-body/login-body.component.ts
@@ -1,17 +1,25 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
-import { log } from 'console';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-login-body',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './login-body.component.html',
   styleUrl: './login-body.component.css'
 })
 export class LoginBodyComponent {
+  loginForm : FormGroup;
 
-  constructor(public authService : AuthService) {
-
+  constructor(public authService : AuthService, private fb : FormBuilder) {
+    this.loginForm = this.fb.group({
+      username: ["", [Validators.required, Validators.minLength(3)]],
+      password: ["", [Validators.required, Validators.minLength(6)]],
+    });
   }
 
   goToLoginPage() {
@@ -20,6 +28,11 @@ export class LoginBodyComponent {
   }
   goToRegisterPage() {
     this.authService.goRegisterPage();
+  }
+
+  loginBtnClick() {
+    console.log(444);
+    
   }
 
 }
--- src/app/components/register-body/register-body.component.ts
+++ src/app/components/register-body/register-body.component.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-register-body',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './register-body.component.html',
   styleUrl: './register-body.component.css'
 })
 export class RegisterBodyComponent {
-
-  constructor(public authService : AuthService) {}
+    registerForm : FormGroup;
+  constructor(public authService : AuthService, private fb : FormBuilder) {
+    this.registerForm = this.fb.group({
+      username: ["", [Validators.required, Validators.minLength(3)]],
+      password: ["", [Validators.required, Validators.minLength(6)]],
+      passwordRepeat: ["", [Validators.required]],
+    });
+  }
 
   goToLoginPage() {
     this.authService.goLoginPage();
