b33f45b555be01d017582514119873a944a3f378
narekavdalyan <narek.avdalyan.2021@mail.ru>
2025-07-30 00:39:24 +0400

    Registered users array, registered user validation and login (succesful login  or reject login)

--- src/app/components/login-body/login-body.component.ts
+++ src/app/components/login-body/login-body.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+
 
 
 @Component({
@@ -13,14 +14,27 @@ import { RouterModule } from '@angular/router';
   templateUrl: './login-body.component.html',
   styleUrl: './login-body.component.css'
 })
-export class LoginBodyComponent {
+export class LoginBodyComponent implements OnInit {
   loginForm : FormGroup;
+
+  users_array : { username: string; password: string }[] =  [];
+  
+  
 
   constructor(public authService : AuthService, private fb : FormBuilder) {
     this.loginForm = this.fb.group({
       username: ["", [Validators.required, Validators.minLength(3)]],
       password: ["", [Validators.required, Validators.minLength(6)]],
     });
+    // console.log(this.users_array);
+    
+  }
+
+    ngOnInit(): void {
+    if (typeof window !== 'undefined') {
+      const storedUsers = localStorage.getItem('registeredUsers');
+      this.users_array = storedUsers ? JSON.parse(storedUsers) : [];
+    }
   }
 
   goToLoginPage() {
@@ -31,10 +45,27 @@ export class LoginBodyComponent {
   }
 
   loginBtnClick() {
-    this.authService.loginBtnClick();
-  }
+    // this.authService.loginBtnClick();
+
+    let {username, password} = this.loginForm.value;
+
+    let user = this.users_array.find(
+      u => u.username === username && u.password === password
+    );
+
+    if(user) {
+      alert(`Hello ${user.username}`);
+      setTimeout(() => {
+        this.authService.loginBtnClick();
+      }, 3000);
+     
+    } else {
+      alert('ERROR : username or password is incorrect');
+    };
+
+  };
   OpenForgotPasswordPage() {
     this.authService.OpenForgotPasswordPage();
-  }
+  };
 
 }
--- src/app/components/register-body/register-body.component.ts
+++ src/app/components/register-body/register-body.component.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
-import { FormBuilder, FormGroup, Validators,ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { log } from 'node:console';
+
+interface User {
+  id: number,
+  username: string,
+  password: string,
+};
 
 
 @Component({
@@ -10,9 +17,23 @@ import { CommonModule } from '@angular/common';
   templateUrl: './register-body.component.html',
   styleUrl: './register-body.component.css'
 })
+
+
+
+
 export class RegisterBodyComponent {
-    registerForm : FormGroup;
-  constructor(public authService : AuthService, private fb : FormBuilder) {
+
+
+
+  registerForm: FormGroup;
+  registeredUsers: User[] = localStorage.getItem('registeredUsers') ? JSON.parse(localStorage.getItem('registeredUsers')!) : [];
+  registeredUser: User = {
+    id: 0,
+    username: '',
+    password: '',
+  };
+
+  constructor(public authService: AuthService, private fb: FormBuilder) {
     this.registerForm = this.fb.group({
       username: ["", [Validators.required, Validators.minLength(3)]],
       password: ["", [Validators.required, Validators.minLength(6)]],
@@ -21,14 +42,31 @@ export class RegisterBodyComponent {
   }
 
   goToLoginPage() {
-    if(this.registerForm.get('passwordRepeat')?.value === this.registerForm.get('password')?.value) {
+    if (this.registerForm.get('passwordRepeat')?.value === this.registerForm.get('password')?.value) {
       this.authService.goLoginPage();
-    }
+    };
 
   };
+
+  registerTheUser() {
+    if (this.registerForm.get('passwordRepeat')?.value === this.registerForm.get('password')?.value) {
+
+      const newUser: User = {
+        id: Math.floor(Math.random() * 1000),
+        username: this.registerForm.get('username')?.value,
+        password: this.registerForm.get('password')?.value
+      };
+
+      this.registeredUsers.push(newUser);
+      localStorage.setItem('registeredUsers', JSON.stringify(this.registeredUsers));
+
+      console.log(localStorage.getItem('registeredUsers'));
+      console.log(this.registeredUsers);
+      
+    }
+  }
 
   goToRegisterPage() {
     this.authService.goRegisterPage();
   }
-
 }
